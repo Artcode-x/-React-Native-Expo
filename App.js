b016c616cae4bc958fb4c4eac49061fbf5025b7a
Alex <aironer@yandex.ru>
2024-07-09 19:24:47 +0300

    работа с инпутом

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
+import { Keyboard, Text, TextInput, View } from "react-native"
 import { styles } from "./App.css"
+import { useState } from "react"
 
 export default function App() {
+  const [name, setName] = useState()
+  console.log(name)
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder={"Введите Пароль"}
+        keyboardType={"numeric"}
+      />
       <StatusBar style="auto" />
     </View>
   )
