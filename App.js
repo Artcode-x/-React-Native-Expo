5c92cb9786f8dc141fed1299bd51a17d8ae771be
Alex <aironer@yandex.ru>
2024-07-09 20:30:45 +0300

    1. Использование индикатора загрузки как лоадера - ActivityIndicator 2. Обработка ошибки с сервера

--- App.js
+++ App.js
@@ -1,37 +1,46 @@
 import { StatusBar } from "expo-status-bar"
-import { FlatList, Keyboard, Text, TextInput, View } from "react-native"
+import { ActivityIndicator, Alert, FlatList, Keyboard, Text, TextInput, View } from "react-native"
 import { styles } from "./App.css"
 import { useEffect, useState } from "react"
 
 export default function App() {
   const [name, setName] = useState()
   const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    setIsLoading(true)
     fetch("https://api.chucknorris.io/jokes/search?query=fet")
       .then((res) => res.json())
       .then((res) => setData(res.result))
-  })
+      .catch((err) => alert(err))
+      .finally(() => setIsLoading(false))
+  }, [])
 
   // console.log(data)
+  // Alert.alert("Ошибка с сервера", "Текст ошибки")
 
+  if (isLoading) {
+    return (
+      <ActivityIndicator size={"large"} style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />
+    )
+  }
   return (
     <View style={styles.container}>
+      {/* ActivityIndicator - индикатор загрузки */}
+      <ActivityIndicator size={"large"} color={"wheat"} />
       {/* Flatlist - используется для вывода контента со скроллом / Предоставляет интерфейс для вывода списков */}
-
       {/* renderItem - используется аналогично методу map, пишем туда выводимые данные*/}
       <FlatList
         data={data}
         renderItem={({ item }) => <Text>{item.value}</Text>}
         keyExtractor={(item) => item.id}
       />
-
       {/* {data.map((el) => (
         <Text key={el.id} style={styles.container}>
           {el.value}
         </Text>
       ))} */}
-
       <Text style={styles.text}>Hello World!</Text>
       <TextInput
         value={name}
