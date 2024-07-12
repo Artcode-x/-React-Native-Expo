ccc2f73c89310c871242ef363430446f8d2a45ca
Alex <aironer@yandex.ru>
2024-07-12 17:22:33 +0300

    Рефакторинг 1. Чистка от комментариев

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,22 +1,10 @@
 import { useEffect, useState } from "react"
-import {
-  ActivityIndicator,
-  Alert,
-  FlatList,
-  Keyboard,
-  Text,
-  TextInput,
-  View,
-  RefreshControl,
-  Button,
-  TouchableOpacity,
-} from "react-native"
+import { ActivityIndicator, FlatList, Text, TextInput, View, Button, TouchableOpacity } from "react-native"
 
 import { styles } from "./Main.styles"
 import { StatusBar } from "expo-status-bar"
 import { Joke } from "./components/Joke/Joke"
 
-// вместо хука реакта useNavigate, пишем navigation
 export const Main = ({ navigation }) => {
   const [name, setName] = useState()
   const [data, setData] = useState()
@@ -35,8 +23,6 @@ export const Main = ({ navigation }) => {
     fetchPromise()
   }, [])
 
-  //   Alert.alert("Ошибка с сервера", "Текст ошибки")
-
   if (isLoading) {
     return (
       <ActivityIndicator size={"large"} style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />
@@ -48,23 +34,14 @@ export const Main = ({ navigation }) => {
         <Button
           style={styles.button}
           title="Перейти на страницу о нас"
-          //   параметром для перехода указывается атрибут AboutScreen, который мы указали в App при роутинге = какая страница
           onPress={() => navigation.navigate("AboutScreen")}
         />
-        {/* ActivityIndicator - индикатор загрузки */}
         <ActivityIndicator size={"large"} color={"wheat"} />
-        {/* Flatlist - используется для вывода контента со скроллом / Предоставляет интерфейс для вывода списков */}
-        {/* renderItem - используется аналогично методу map, пишем туда выводимые данные*/}
         <FlatList
-          // указываем флаг с пом-ю которого refreshcontrol понимает показывать или нет наши данные
           refreshing={isLoading}
-          // далее указываем что будем обновлять:
           onRefresh={fetchPromise}
           keyExtractor={(item) => item.id}
           data={data}
-          // было: renderItem={({ item }) => <Text>{item.value}</Text>}
-          // какой конкретно компонент мы должны отрисовать:
-          //   renderItem={({ item }) => <Joke text={item.value} navigation={navigation} id={item.id} />}
           renderItem={({ item }) => (
             <TouchableOpacity
               onPress={() => navigation.navigate("JokeScreen", { id: item.id, title: item.created_at })}
@@ -73,7 +50,6 @@ export const Main = ({ navigation }) => {
             </TouchableOpacity>
           )}
         />
-        {/* // <Joke text={item.value} navigation={navigation} id={item.id} />} */}
         <Text style={styles.text}>Hello World!</Text>
         <TextInput
           value={name}
--- src/pages/Routing/Routing.jsx
+++ src/pages/Routing/Routing.jsx
@@ -5,13 +5,10 @@ import { NavigationContainer } from "@react-navigation/native"
 import { JokeScreen } from "../../../src/pages/JokeScreen/JokeScreen"
 
 export const Routing = () => {
-  // условно stack - это routes
   const Stack = createNativeStackNavigator()
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* name - обязательный атрибут, условно наш путь/идентификатор для отображаемого компонента/ возм-но название стр-цы, и рядом указывается сам компонент который будет отображаться */}
-        {/* название страницы можно менять с пом-ю атрибута option */}
         <Stack.Screen name={"MainScreen"} component={Main} options={{ title: "Главная страница" }} />
         <Stack.Screen name={"AboutScreen"} component={About} options={{ title: "О Нас" }} />
         <Stack.Screen name={"JokeScreen"} component={JokeScreen} options={{ title: "Шутка" }} />
